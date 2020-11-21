02cb0278fc96b719ca2e8cd028b0423a649f1587
JenkenK <jenkenkwok@gmail.com>
2020-11-21 19:29:18 +0000

    basic game of blackjack working, added method displayPlayerHand, added method hit, added loop to hit or stick and refactor results output

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,6 +1,23 @@
 import java.util.Scanner;
 
 public class Runner {
+
+    // method that displays and output player's hand
+    public static void displayPlayerHand(Game game, int index){
+        // prints out prompt of "--players name-- has: "
+        String playerCards = String.format("%s has:", game.getPlayers().get(index).getName());
+        System.out.println(playerCards);
+        // loops through players cards in hand and prints it out cards and total hand value"
+        for(int i = 0; i < game.getPlayers().get(index).cardCount(); i ++){
+            System.out.println(game.getPlayers().get(index).showCard(i));
+        }
+        System.out.printf("Hand total: %s%n", game.getPlayers().get(index).handTotal());
+    }
+
+    public static void hit(Game game, Deck deck){
+        game.getPlayers().get(1).takeCard(deck.dealOne());
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -23,50 +40,51 @@ public class Runner {
         // gets player's name from 2nd index of players in game array
 //        System.out.println(game.getPlayers().get(1).getName());
 
-
-        // prints out prompt of "--players name-- has: "
-        String playerCards = String.format("%s has:", game.getPlayers().get(1).getName());
-        System.out.println(playerCards);
-        // loops through players cards in hand and prints it out cards and total hand value"
-        for(int i = 0; i < game.getPlayers().get(1).cardCount(); i ++){
-            System.out.println(game.getPlayers().get(1).showCard(i));
-        }
-        System.out.printf("Hand total: %s%n", game.getPlayers().get(1).handTotal());
+        System.out.println("----- PLAYERS CARDS -----");
+        displayPlayerHand(game, 1);
 
 
         // this checks if their hand is less than 21 and asks if they want to hit or stick
-        if (game.getPlayers().get(1).handTotal() < 21){
+        while (game.getPlayers().get(1).handTotal() < 21) {
             System.out.println("Hit or Stick? (input H/h or S/s)");
             String playerMove = scanner.next();
             if (playerMove.equals("h") || playerMove.equals("H")){
-                do {
-                    Card cardFromHit = deck.dealOne();
-                    game.getPlayers().get(1).takeCard(cardFromHit);
-                    System.out.println(cardFromHit.cardName());
-                    System.out.println(game.getPlayers().get(1).handTotal());
-                }
-                while(game.getPlayers().get(1).handTotal() < 21);
+                hit(game, deck);
+                System.out.println("----- PLAYERS CARDS -----");
+                displayPlayerHand(game,1 );
+            } else if (playerMove.equals("s") || playerMove.equals("S")){
+                break;
+            } else {
+                System.out.println("Please input H/h or S/s. ");
             }
-//            System.out.println(game.getPlayers().get(1).handTotal());
         }
 
-//        for(Player player: game.getPlayers()){
-//            String output = String.format("%s has:", player.getName());
-//            System.out.println(output);
-//            for(int i = 0; i < player.cardCount(); i ++){
-//                System.out.println(player.showCard(i));
+
+
+// probs can get rid of this
+//        if (game.getPlayers().get(1).handTotal() < 21){
+//            System.out.println("Hit or Stick? (input H/h or S/s)");
+//            String playerMove = scanner.next();
+//            if (playerMove.equals("h") || playerMove.equals("H")){
+//                hit(game, deck);
 //            }
-//            System.out.printf("Hand total: %s%n", player.handTotal());
+//            displayPlayerHand(game);
+//            if (game.getPlayers().get(1).handTotal() > 21){
+//                game.checkWinner();
+//            }
 //        }
 
+        System.out.println("----- DEALERS CARDS -----");
+        displayPlayerHand(game, 0);
 
-//        if(game.checkDraw()){
-//            System.out.println("It's a draw!");
-//        } else {
-//            Player winner = game.checkWinner();
-//            String winnerName = winner.getName();
-//            String output = String.format("%s wins!", winnerName);
-//            System.out.println(output);
-//        }
+        System.out.println("----- RESULTS -----");
+        if(game.checkDraw()){
+            System.out.println("It's a draw!");
+        } else {
+            Player winner = game.checkWinner();
+            String winnerName = winner.getName();
+            String output = String.format("%s wins!", winnerName);
+            System.out.println(output);
+        }
     }
 }

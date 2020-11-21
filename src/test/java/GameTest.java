e97523ccf5469c5cd52c95cde932b39024e8aaf1
JenkenK <jenkenkwok@gmail.com>
2020-11-21 16:48:42 +0000

    created gameTest and setup of set, added first test of gameHasPlayers

---
+++ src/test/java/GameTest.java
@@ -0,0 +1,33 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class GameTest {
+
+    Player player;
+    Player dealer;
+    Deck deck;
+    Game game;
+    Card highCard;
+    Card lowCard;
+
+    @Before
+    public void setUp(){
+        player = new Player("Jenken");
+        dealer = new Player("Dealer");
+        deck = new Deck();
+        game = new Game(deck);
+        game.addPlayer(player);
+        game.addPlayer(dealer);
+
+        highCard = new Card(Suit.SPADES, Rank.KING);
+        lowCard = new Card(Suit.SPADES, Rank.TWO);
+    }
+
+    @Test
+    public void gameHasPlayers(){
+        assertEquals(2, game.playerCount());
+    }
+
+}
